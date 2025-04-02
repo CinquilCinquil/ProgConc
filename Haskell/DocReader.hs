7d45aa429e8a051700c8a923a1a24a1952aeadd9
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2025-04-02 17:15:22 -0300

    refactor(Haskell): Reorganized code

--- Haskell/DocReader.hs
+++ Haskell/DocReader.hs
@@ -4,6 +4,7 @@ import Control.Monad
 import Control.Exception
 import Data.Text (Text, unpack)
 import Pdf.Document
+import Utils
 
 -- Obs: you gotta use ':set -package text' before loading
 
@@ -49,44 +50,17 @@ tokenizer (x:xs) = conct_to_head x (tokenizer xs)
 clean_str :: String -> String
 clean_str s = remove_str "\\" $ remove_str "\"" $ (remove_sequence_of_str ["\\", "n"] s)
 
-conct_to_head :: Char -> [String] -> [String]
-conct_to_head c [] = [[c]]
-conct_to_head c (x:xs) = ([c] ++ x) : xs
-
--- whether X appears as a substring in the start of Y
-fits_at_ys_start :: String -> String -> Bool
-fits_at_ys_start [] _ = True
-fits_at_ys_start _ [] = False
-fits_at_ys_start (x:xs) (y:ys) = if x == y then fits_at_ys_start xs ys else False
-
-remove_str :: String -> String -> String
-remove_str [] xs = xs
-remove_str _ [] = []
-remove_str (x:[]) (y:ys) = if x == y then ys else y:(remove_str (x:[]) ys)
-remove_str (x:xs) (y:ys) = if fits_at_ys_start (x:xs) (y:ys)
-                              then remove_str xs ys else y:(remove_str (x:xs) ys)
-
-remove_sequence_of_str :: [String] -> String -> String
-remove_sequence_of_str [] xs = xs
-remove_sequence_of_str _ [] = []
-remove_sequence_of_str ((x':[]):[]) (y:ys) = if x' == y then ys else y:(remove_sequence_of_str ((x':[]):[]) ys)
-remove_sequence_of_str ((x':[]):xs) (y:ys) = if x' == y then
-                remove_sequence_of_str xs ys else y:(remove_sequence_of_str ((x':[]):xs) ys)
-remove_sequence_of_str ((x':xs'):[]) (y:ys) = if x' == y then ys else y:(remove_sequence_of_str (xs':[]) ys)
-remove_sequence_of_str ((x':xs'):xs) (y:ys) = if fits_at_ys_start (x':xs') (y:ys)
-                then remove_sequence_of_str (xs':xs) ys else y:(remove_sequence_of_str ((x':xs'):xs) ys)
-
 tokenizePages :: PageNode -> Int -> IO String
 tokenizePages _ (-1) = return ""
 tokenizePages rootNode count = do
-                                txt1 <- tokenizePages rootNode (count-1)
-                                page <- pageNodePageByNum rootNode count
-                                result <- try (pageExtractText page) :: IO (Either SomeException Text)
-                                case result of
-                                    Left ex -> do
-                                        putStrLn $
-                                          "    !! Failed reading page " ++ (show count)
-                                        return txt1
-                                    Right val -> do
-                                        when ((count `mod` 100) == 0) $ print "Read 100 Pages"
-                                        return $ txt1 ++ (show val)
+            txt1 <- tokenizePages rootNode (count-1)
+            page <- pageNodePageByNum rootNode count
+            result <- try (pageExtractText page) :: IO (Either SomeException Text)
+            case result of
+                Left ex -> do
+                    putStrLn $
+                        "    !! Failed reading page " ++ (show count)
+                    return txt1
+                Right val -> do
+                    when ((count `mod` 100) == 0) $ print "Read 100 Pages"
+                    return $ txt1 ++ (show val)
--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -5,6 +5,7 @@ import System.Directory
 import Pdf.Document
 import DocReader
 import BM25
+import Utils
 
 -- Obs: you gotta use ':set -package directory' before loading
 
@@ -13,22 +14,14 @@ filepath :: String = "../data/pdfs/"
 
 main = do
        files <- getDirectoryContents filepath
+       -- processing docs
        let pdf_files = filter (is_file_type "pdf") (map (filepath ++) files)
-       pre_documents <- io_comm $ map tokenizeDoc pdf_files
+       pre_documents <- mapM tokenizeDoc pdf_files
        let documents = filter (\xs -> if xs == [] then True else False) pre_documents
+       -- parameters
        let nDocs :: Double = fromIntegral (length documents)
        let avgdl :: Double = get_avgdl nDocs documents
+       -- result
        let score = doc_score (nDocs, avgdl) documents (head documents) query
        print score
 
-io_comm :: [IO a] -> IO [a]
-io_comm [] = return []
-io_comm (x:xs) = do
-             x' <- x
-             xs' <- io_comm xs
-             return (x': xs')
-
-is_file_type :: String -> String -> Bool
-is_file_type [] _ = True
-is_file_type _ [] = False
-is_file_type xs ys = fits_at_ys_start (reverse $ "." ++ xs) (reverse ys)

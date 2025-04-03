a0d0d318bbad7bc452c0ef43be16d3b8ea4067e4
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2025-04-03 16:14:59 -0300

    feat(Java): Add concurrency to get_token_frequency

--- Java/BM25/src/main/java/Models/Document.java
+++ Java/BM25/src/main/java/Models/Document.java
@@ -5,13 +5,18 @@ import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument; 
 import org.apache.pdfbox.text.PDFTextStripper;
 
+import java.util.ArrayList;
 import java.util.StringTokenizer;
+
+import static java.lang.Math.max;
 
 public class Document {
 
     private String my_text, name;
     private int n_tokens;
     private final String DEFAULT_SEPARATION = " ";
+    private final int block_size = 5000;
+    private int n_blocks;
 
     public Document(String filepath) {
 
@@ -21,7 +26,9 @@ public class Document {
             PDFTextStripper pdfStripper = new PDFTextStripper();
             this.my_text = pdfStripper.getText(document);
 
-            this.n_tokens = get_tokenizer().countTokens();
+            this.n_tokens = (new StringTokenizer(this.my_text, DEFAULT_SEPARATION)).countTokens();
+
+            this.n_blocks = max(n_tokens / block_size, 1);
 
             System.out.println("Successfully read the file " + filepath);
 
@@ -32,8 +39,14 @@ public class Document {
 
     }
 
-    private StringTokenizer get_tokenizer() {
-        return new StringTokenizer(my_text, DEFAULT_SEPARATION);
+    private ArrayList<StringTokenizer> get_tokenizers() {
+        ArrayList<StringTokenizer> tokenizers = new ArrayList<StringTokenizer>();
+        for (int i = 0; i < n_blocks; i++) {
+            int block_end = i < n_blocks - 1 ? block_size * (i + 1) : my_text.length();
+            String my_text_block = my_text.substring(block_size * i, block_end);
+            tokenizers.add(new StringTokenizer(my_text_block, DEFAULT_SEPARATION));
+        }
+        return tokenizers;
     }
 
     public int get_n_tokens() {
@@ -42,22 +55,47 @@ public class Document {
 
     public int get_token_frequency(String token) {
 
-        int total = 0;
-        StringTokenizer st = get_tokenizer();
+        /*
+            Reads each chunk of the text in a separate thread
+         */
 
-        while (st.hasMoreTokens()) {
-            if (st.nextToken().equalsIgnoreCase(token)) {
-                total ++;
+        class Counter {
+            int total = 0;
+            public synchronized void increment() {
+                total++;
             }
         }
 
-        return total;
+        Counter counter = new Counter();
+
+        WorkerManager workerManager = new WorkerManager();
+        ArrayList<StringTokenizer> sts = get_tokenizers();
+
+        for (StringTokenizer st : sts) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (st.hasMoreTokens()) {
+                        if (st.nextToken().equalsIgnoreCase(token)) {
+                            counter.increment();
+                        }
+                    }
+                }
+            });
+            t.start();
+            workerManager.addWorker(t);
+        }
+
+        while (workerManager.workers_alive()) {}
+
+        return counter.total;
 
     }
 
+    // Parallel this
     public boolean has_token(String token) {
 
-        StringTokenizer st = get_tokenizer();
+        StringTokenizer st = new StringTokenizer(my_text, DEFAULT_SEPARATION);
 
         while (st.hasMoreTokens()) {
             if (st.nextToken().equalsIgnoreCase(token)) {

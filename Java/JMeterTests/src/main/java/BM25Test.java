deed6aa208e0fbb7ff37bb5269195243234387b7
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2025-05-15 21:34:14 -0300

    test(Java, BM25): Add JMeter tests

---
+++ Java/JMeterTests/src/main/java/BM25Test.java
@@ -0,0 +1,58 @@
+import BM25v.Models.BM25;
+import BM25v.Models.DocumentData;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+import java.io.Serializable;
+
+public class BM25Test extends AbstractJavaSamplerClient implements Serializable {
+    @Override public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
+        String param_main_n_threads = javaSamplerContext.getParameter("main_n_threads");
+        String param_bm25_n_threads = javaSamplerContext.getParameter("bm25_n_threads");
+        String param_docdata_n_threads = javaSamplerContext.getParameter("docdata_n_threads");
+        String param_docdata_block_size = javaSamplerContext.getParameter("docdata_block_size");
+        String param_query = javaSamplerContext.getParameter("query");
+        String param_path = javaSamplerContext.getParameter("path");
+
+        SampleResult result = new SampleResult();
+        result.sampleStart();
+        result.setSampleLabel("BM25 Test Sample");
+
+        BM25v.Main.n_threads = Integer.parseInt(param_main_n_threads);
+        BM25v.Main.query_text = param_query;
+        BM25v.Main.path = param_path;
+        BM25.n_threads = Integer.parseInt(param_bm25_n_threads);
+        DocumentData.n_threads = Integer.parseInt(param_docdata_n_threads);
+        DocumentData.block_size = Integer.parseInt(param_docdata_block_size);
+
+        BM25v.Main.main(null);
+
+        if (BM25v.Main.total_processed_docs == 0) {
+            result.sampleEnd();
+            result.setResponseCode("500");
+            result.setResponseMessage("NOK " + BM25v.Main.total_processed_docs);
+            result.setSuccessful(false);
+        }
+        else {
+            result.sampleEnd();
+            result.setResponseCode("200");
+            result.setResponseMessage("OK " + BM25v.Main.total_processed_docs);
+            result.setSuccessful(true);
+        }
+
+        return result;
+    }
+
+    @Override public Arguments getDefaultParameters() {
+        Arguments defaultParameters = new Arguments();
+        defaultParameters.addArgument("main_n_threads", String.valueOf(MetaInfo.main_n_threads));
+        defaultParameters.addArgument("bm25_n_threads", String.valueOf(MetaInfo.bm25_n_threads));
+        defaultParameters.addArgument("docdata_n_threads", String.valueOf(MetaInfo.docdata_n_threads));
+        defaultParameters.addArgument("docdata_block_size", String.valueOf(MetaInfo.docdata_block_size));
+        defaultParameters.addArgument("query", MetaInfo.query_text);
+        defaultParameters.addArgument("path", MetaInfo.path);
+        return defaultParameters;
+    }
+}
---
+++ Java/JMeterTests/src/main/java/MetaInfo.java
@@ -0,0 +1,10 @@
+public class MetaInfo {
+
+    public static final String path = "../../data/subset/";
+    public static final String query_text = "partial function";
+    public static final int main_n_threads = 40;
+    public static final int bm25_n_threads = 40;
+    public static final int docdata_n_threads = 40;
+    public static final int docdata_block_size = 40;
+
+}
